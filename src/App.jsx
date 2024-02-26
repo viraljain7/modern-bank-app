0eb847fcd9a9dced5bbf5b85580563039bec6ce1
viraljain7 <veerljain1234@gmail.com>
2024-02-26 18:08:52 +0530

    Uploaded: file setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0);
+  const count = "hello guys"
+
+  return (
+    <>
+      <div className="text-red-500 text-3xl">
+        {count}
+      </div>
+    </>
+  )
+}
+
+export default App
